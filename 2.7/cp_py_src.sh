62a7d46beecb8b3a6d57cb11b2bbf9e61a4480cd
Luciano Ramalho <luciano@ramalho.org>
2012-01-19 13:56:31 -0200

    criacao de dir Include e copia do Include/patchlevel.h nos scripts que usei para iniciar o trabalho

--- 2.7/cp_py_src.sh
+++ 2.7/cp_py_src.sh
@@ -5,4 +5,7 @@ cp $PYTHON_SRC/LICENSE .
 cp $PYTHON_SRC/Grammar/Grammar Grammar/
 cp $PYTHON_SRC/Lib/test/exception_hierarchy.txt Lib/test/
 cp $PYTHON_SRC/Parser/Python.asdl Parser/
-cp -Rv $PYTHON_SRC/Doc/ Doc/
+cp $PYTHON_SRC/Include/patchlevel.h Include/
+# do not uncomment this unless you know all translations will be overwritten,
+# and that is what you really want to do
+# cp -Rv $PYTHON_SRC/Doc/ Doc/
